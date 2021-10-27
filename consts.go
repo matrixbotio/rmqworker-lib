03b167045a540ff4fece942c710b8659b1a21704
Sagleft <sagleft@mfc.click>
2021-10-27 19:17:56 +0300

    add reconnection timeout

--- consts.go
+++ consts.go
@@ -3,6 +3,7 @@ package rmqworker
 const (
 	reconnectionAttemptsNumber = 3
 	reconnectAfterSeconds      = 2
+	reconnectionTimeout        = 4  // seconds
 	waitingBetweenAttempts     = 60 // seconds
 	cronConnectionCheckTimeout = 3  // minutes
 )
