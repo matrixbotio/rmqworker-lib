f4619a1cbfc1c2a52cac5e669c2aab1c5a3dfb47
Sagleft <sagleft@mfc.click>
2021-11-01 15:22:06 +0200

    Update connection.go
    Co-authored-by: Nikita Zhdanov <wsleepwalker@gmail.com>

--- connection.go
+++ connection.go
@@ -144,7 +144,7 @@ func (r *RMQWorker) reconnect(callback connFunc, connectionDescription string) {
 			if isTimeIsUP {
 				r.logger.Warn(constants.Error(
 					"SERVICE_CONN_ERR",
-					r.getLogWorkerName()+"the connection went into timeout",
+					r.getLogWorkerName()+" connection timeout",
 				))
 			}
 			time.Sleep(reconnectAfterSeconds * time.Second)
