f292f1977c80788a07e790b25d2220a986651b7d
Sagleft <sagleft@inbox.ru>
2022-05-12 20:40:12 +0300

    queue delete method

--- queues.go
+++ queues.go
@@ -1,23 +1,36 @@
 package rmqworker
 
+import "github.com/matrixbotio/constants-lib"
+
 // DeleteQueues - delete RMQ queues.
 // map[manager name] -> array of queue names
-/*func (r *RMQHandler) DeleteQueues(queueNames map[string][]string) APIError {
+func (r *RMQHandler) DeleteQueues(queueNames map[string][]string) APIError {
 	for managerName, queueNames := range queueNames {
 		for _, queueName := range queueNames {
-			_, err := r.Connections.Publish.Channel.QueueDelete(
-				queueName, // queue name
-				false,     // if unused
-				false,     // if empty
-				true,      // no-wait
-			)
+			err := r.queueDelete(managerName, queueName)
 			if err != nil {
-				return constants.Error(
-					"SERVICE_REQ_FAILED",
-					"failed to delete "+managerName+" queue: "+err.Error(),
-				)
+				return err
 			}
 		}
 	}
 	return nil
-}*/
+}
+
+func (r *RMQHandler) queueDelete(managerName, queueName string) APIError {
+	r.rlock()
+	defer r.runlock()
+
+	_, err := r.channelKeeper.Channel().QueueDelete(
+		queueName, // queue name
+		false,     // if unused
+		false,     // if empty
+		true,      // no-wait
+	)
+	if err != nil {
+		return constants.Error(
+			"SERVICE_REQ_FAILED",
+			"failed to delete "+managerName+" queue: "+err.Error(),
+		)
+	}
+	return nil
+}
